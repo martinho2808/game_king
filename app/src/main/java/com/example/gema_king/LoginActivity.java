0d8facd70724c6a321869c7a342f3f8a6c8a73de
Eden Fung <Michealbox1996@gmail.com>
2025-03-29 00:48:47 +0800

    Update

--- app/src/main/java/com/example/gema_king/LoginActivity.java
+++ app/src/main/java/com/example/gema_king/LoginActivity.java
@@ -15,27 +15,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 
 public class LoginActivity extends AppCompatActivity {
-    private Button btnLogin;
     private static final String TAG = "LoginActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
         Log.i(TAG, "Access Login Page");
-        btnLogin = findViewById(R.id.login_btn_signup);
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "Click Sign Up");
-                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(i);
-                Toast.makeText(LoginActivity.this, "Welcome to Register", Toast.LENGTH_SHORT).show();
-            }
-        });
+        Toast.makeText(LoginActivity.this, "Welcome to Login", Toast.LENGTH_SHORT).show();
     }
-
 }
--- app/src/main/java/com/example/gema_king/MainActivity.java
+++ app/src/main/java/com/example/gema_king/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.gema_king;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,28 +14,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
+    Button sign_up = (Button) findViewById(R.id.login_btn_signup);
+    Button login = (Button) findViewById(R.id.login_btn_login);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        Toast.makeText(this, "Welcome to Game King", Toast.LENGTH_LONG).show();
 
+        sign_up.setOnClickListener(this);
+        login.setOnClickListener(this);
+    }
 
-
-
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        if(v.getId() == sign_up.getId()) {
+            i = new Intent(MainActivity.this, RegisterActivity.class);
+        } else {
+            i = new Intent(MainActivity.this, LoginActivity.class);
+        }
+        startActivity(i);
     }
 }
