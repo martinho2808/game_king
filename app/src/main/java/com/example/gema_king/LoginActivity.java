6a03079add4ee6f87e20869410afbcaf866bfc9f
Eden Fung <Michealbox1996@gmail.com>
2025-03-29 02:50:29 +0800

    add

--- app/src/main/java/com/example/gema_king/LoginActivity.java
+++ app/src/main/java/com/example/gema_king/LoginActivity.java
@@ -21,6 +21,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         Log.i(TAG, "Access Login Page");
         Toast.makeText(LoginActivity.this, "Welcome to Login", Toast.LENGTH_SHORT).show();
