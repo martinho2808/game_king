f008879578a42d98991435bccd03233694a7e7ea
Eden Fung <Michealbox1996@gmail.com>
2025-03-29 12:46:50 +0800

    Adding database

---
+++ app/src/main/java/com/example/gema_king/DatabaseHelper.java
@@ -0,0 +1,29 @@
+package com.example.gema_king;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "MyDatabase.db";
+    private static final String TABLE_NAME = "MyTable";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
